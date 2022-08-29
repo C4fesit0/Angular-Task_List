b5535bfbefeeb5e9f54504f232681005d518095f
Alan Lazo <al4nlaz0@gmail.com>
2022-08-29 01:00:47 -0300

    add toggle add form function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title:string="Task Tracker"
-  constructor() { }
+  title:string="Task Tracker";
+  showAddTask:boolean = false;
+  subscription : Subscription;
+
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe(value => this.showAddTask=value);
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() :void{
-    console.log("toggle")
+    this.uiService.toggleAddTask();
   }
 
 }
